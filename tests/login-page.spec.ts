767ec0eb73e7b63ec14a8b29df36091ab867792a
Meda <mac@macs-MacBook-Pro.local>
2024-02-16 20:52:49 +0200

    add screenshot tests for login page

--- tests/login-page.spec.ts
+++ tests/login-page.spec.ts
@@ -1,27 +1,51 @@
 import { test, expect } from "@playwright/test";
 
-test("user logs in", async ({ page }) => {
-  await page.goto("/");
+test.describe("error cases", () => {
+  test.describe("happy path", () => {
+    test("user logs in", async ({ page }) => {
+      await page.goto("/");
+      await expect(page).toHaveScreenshot("login-page.png");
 
-  const emailInput = page.getByPlaceholder("email");
-  const passwordInput = page.getByPlaceholder("password");
-  const loginButton = page.getByRole("button", { name: "Login" });
+      const emailInput = page.getByPlaceholder("email");
+      const passwordInput = page.getByPlaceholder("password");
+      const loginButton = page.getByRole("button", { name: "Login" });
 
-  await emailInput.fill("test@test.com");
-  await passwordInput.fill("testtest");
-  await loginButton.click();
+      await emailInput.fill("test@test.com");
+      await passwordInput.fill("testtest");
+      await loginButton.click();
 
-  await expect(page).toHaveURL("/profile");
+      await expect(page).toHaveURL("/profile");
+      await expect(page).toHaveScreenshot("profile-page.png");
+    });
+  });
+
+  test.describe("errors", () => {
+    test("user does not exists", async ({ page }) => {
+      await page.goto("/");
+      const emailInput = page.getByPlaceholder("email");
+      const passwordInput = page.getByPlaceholder("password");
+      const loginButton = page.getByRole("button", { name: "Login" });
+
+      await emailInput.fill("incorrect@user.com");
+      await passwordInput.fill("testtest");
+      await loginButton.click();
+
+      await expect(page).toHaveURL("/login");
+      await expect(page).toHaveScreenshot("login-error-no-user.png");
+    });
+
+    test("password is incorrect", async ({ page }) => {
+      await page.goto("/");
+      const emailInput = page.getByPlaceholder("email");
+      const passwordInput = page.getByPlaceholder("password");
+      const loginButton = page.getByRole("button", { name: "Login" });
+
+      await emailInput.fill("test@test.com");
+      await passwordInput.fill("incorrect");
+      await loginButton.click();
+
+      await expect(page).toHaveURL("/login");
+      await expect(page).toHaveScreenshot("login-error-bad-password.png");
+    });
+  });
 });
-
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
