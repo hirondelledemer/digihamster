84b5ff6da7edca3c6c79bc324584244f6f575220
Meda <mac@macs-MacBook-Pro.local>
2024-03-08 01:57:41 +0200

    add context menu to complete task

--- src/app/components/TaskCard/TaskCard.tsx
+++ src/app/components/TaskCard/TaskCard.tsx
@@ -9,6 +9,17 @@ import {
   CardTitle,
 } from "../ui/card";
 import MinimalNote from "../MinimalNote";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from "../ui/context-menu";
+import axios from "axios";
+import useTasks from "@/app/utils/hooks/use-tasks";
+import { updateObjById } from "@/app/utils/common/update-array";
+import { useToast } from "../ui/use-toast";
 
 export const titleTestId = "TaskCard-title-testid";
 
@@ -24,22 +35,71 @@ const TaskCard: FC<TaskCardProps> = ({
   className,
 }): JSX.Element => {
   const { data: projects } = useProjects();
+  const { setData: setTasksData } = useTasks();
+  const { toast } = useToast();
+
   const project = projects.find((p) => p._id === task.projectId);
+
+  // todo: test edit task
+  // show completed task differently
+  // show completed tasks at the bottom
+  // test commandtool
+
+  const editTask = async (completed: boolean) => {
+    try {
+      setTasksData((t) =>
+        updateObjById<Task>(t, task._id, {
+          completed,
+        })
+      );
+      await axios.patch("/api/tasks/events", {
+        taskId: task._id,
+        completed,
+      });
+      toast({
+        title: "Success",
+        description: "Task has been updated",
+      });
+    } catch (e) {
+      toast({
+        title: "Error",
+        description: JSON.stringify(e),
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <div data-testid={testId} className={className}>
-      <Card className="w-[350px] p-0 rounded-md">
-        <CardHeader className="p-4">
-          <CardTitle data-testid={titleTestId}>{task.title}</CardTitle>
-          <CardDescription>
-            <div style={{ color: project?.color }}>{project?.title}</div>
-          </CardDescription>
-        </CardHeader>
-        {task.description && (
-          <CardContent className="px-4 text-xs">
-            <MinimalNote note={task.description} />
-          </CardContent>
-        )}
-      </Card>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <Card className="w-[350px] p-0 rounded-md">
+            <CardHeader className="p-4">
+              <CardTitle data-testid={titleTestId}>{task.title}</CardTitle>
+              <CardDescription>
+                <div style={{ color: project?.color }}>{project?.title}</div>
+              </CardDescription>
+            </CardHeader>
+            {task.description && (
+              <CardContent className="px-4 text-xs">
+                <MinimalNote note={task.description} />
+              </CardContent>
+            )}
+          </Card>
+        </ContextMenuTrigger>
+        <ContextMenuContent className="w-64">
+          {task.completed && (
+            <ContextMenuItem inset onClick={() => editTask(false)}>
+              Undo
+            </ContextMenuItem>
+          )}
+          {!task.completed && (
+            <ContextMenuItem inset onClick={() => editTask(true)}>
+              Complete
+            </ContextMenuItem>
+          )}
+        </ContextMenuContent>
+      </ContextMenu>
     </div>
   );
 };
