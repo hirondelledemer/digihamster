deb96bc01f583da0d5f1c6ecd32b76373ecf1be1
Meda <mac@macs-MacBook-Pro.local>
2024-03-07 20:39:33 +0200

    add task list tests

--- src/app/components/ActiveTaskList/ActiveTaskList.testkit.ts
+++ src/app/components/ActiveTaskList/ActiveTaskList.testkit.ts
@@ -1,7 +1,12 @@
 import { within } from "@/config/utils/test-utils";
 import { taskTestId } from "./ActiveTaskList";
+import { getTaskCardTestkit } from "../TaskCard/TaskCard.testkit";
 
 export const getActiveTaskListTestkit = (component: HTMLElement) => ({
   getComponent: () => component,
   getTasksCount: () => within(component).queryAllByTestId(taskTestId).length,
+  getTaskTitleAt: (index: number) =>
+    getTaskCardTestkit(
+      within(component).queryAllByTestId(taskTestId)[index]
+    ).getTitle(),
 });
