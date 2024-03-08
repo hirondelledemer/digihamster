81dbbd7e721d6636504d67971ecf5db89d6a48a7
Meda <mac@macs-MacBook-Pro.local>
2024-03-08 03:16:04 +0200

    redo task generator

--- src/app/utils/mocks/task.ts
+++ src/app/utils/mocks/task.ts
@@ -1,31 +1,10 @@
 import { Task } from "@/models/task";
 
-export const generateTask: (propertes?: Partial<Task>) => Task = (
-  propertes
+export const generateTask: (i?: number, properties?: Partial<Task>) => Task = (
+  i = 1,
+  properties
 ) => {
   return {
-    _id: "task1",
-    title: "Task 1",
-    description: "task description",
-    completed: false,
-    isActive: true,
-    deleted: false,
-    projectId: "project1",
-    estimate: 0,
-    sortOrder: 0,
-    event: null,
-    completedAt: 0,
-    activatedAt: 0,
-    parentTaskId: null,
-    tags: ["tag1", "tag2"],
-    createdAt: 0,
-    updatedAt: 0,
-    ...propertes,
-  };
-};
-
-export const generateListOfTasks: (count: number) => Task[] = (count) => {
-  return [...Array(count)].map((_v, i) => ({
     _id: `task${i}`,
     title: `Task ${i}`,
     description: `task description ${i}`,
@@ -42,29 +21,18 @@ export const generateListOfTasks: (count: number) => Task[] = (count) => {
     tags: ["tag1", "tag2"],
     createdAt: 0,
     updatedAt: 0,
-  }));
+    ...properties,
+  };
+};
+
+export const generateListOfTasks: (count: number) => Task[] = (count) => {
+  return [...Array(count)].map((_v, i) => generateTask(i));
 };
 
 export const generateCustomTasksList: (taskInfo: Partial<Task>[]) => Task[] = (
   taskInfo
 ) => {
   return taskInfo.map((taskProperties, i) => ({
-    _id: `task${i}`,
-    title: `Task ${i}`,
-    description: `task description ${i}`,
-    completed: false,
-    isActive: true,
-    deleted: false,
-    projectId: "project1",
-    estimate: 0,
-    sortOrder: 0,
-    event: null,
-    completedAt: 0,
-    activatedAt: 0,
-    parentTaskId: null,
-    tags: ["tag1", "tag2"],
-    createdAt: 0,
-    updatedAt: 0,
-    ...taskProperties,
+    ...generateTask(i, taskProperties),
   }));
 };
