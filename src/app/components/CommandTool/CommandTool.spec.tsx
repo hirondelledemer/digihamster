40646119cdeb1e4e3955e96c40f3ac147e4432ba
Meda <mac@macs-MacBook-Pro.local>
2024-03-09 22:05:53 +0200

    remove unused code

--- src/app/components/CommandTool/CommandTool.spec.tsx
+++ src/app/components/CommandTool/CommandTool.spec.tsx
@@ -1,13 +1,14 @@
-import { render } from '@testing-library/react';
-import CommandTool, { CommandToolProps } from './CommandTool';
-import { getCommandToolTestkit } from './CommandTool.testkit';
+import { render } from "@testing-library/react";
+import CommandTool, { CommandToolProps } from "./CommandTool";
+import { getCommandToolTestkit } from "./CommandTool.testkit";
 
-describe('CommandTool', () => {
+// todo show fetch only active tasks
+describe("CommandTool", () => {
   const defaultProps: CommandToolProps = {};
   const renderComponent = (props = defaultProps) =>
     getCommandToolTestkit(render(<CommandTool {...props} />).container);
 
-  it('should render CommandTool', () => {
+  it("should render CommandTool", () => {
     const wrapper = renderComponent();
     expect(wrapper.getComponent()).not.toBe(null);
   });
