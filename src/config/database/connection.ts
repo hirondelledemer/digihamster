b37e168292c6f6c60e69933ff7fe8303786760e1
Meda <mac@macs-MacBook-Pro.local>
2024-03-10 00:19:19 +0200

    restore db connection file

---
+++ src/config/database/connection.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    const mongoUri =
+      process.env.NODE_ENV === "development"
+        ? process.env.MONGO_URI_DEV!
+        : process.env.MONGO_URI!;
+    mongoose.connect(mongoUri);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log("MongoDB connection error" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
