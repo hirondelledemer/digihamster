8b8eade6169d4a6fbd34c8f083d8d2822a5917b1
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2024-08-30 16:35:37 +0300

    chore: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,30 @@
+import type { Preview, ReactRenderer as Renderer } from "@storybook/react";
+import "../src/app/globals.css";
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeByClassName<Renderer>({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "dark",
+    }),
+  ],
+};
+
+export default preview;
--- src/app/components/CalendarEvent/CalendarEvent.stories.ts
+++ src/app/components/CalendarEvent/CalendarEvent.stories.ts
@@ -1,47 +1,49 @@
-// import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-// import CalendarEvent from '.';
-// import { CalendarEventProps } from './CalendarEvent';
+import CalendarEvent from ".";
+import { CalendarEventProps } from "./CalendarEvent";
 
-// const meta: Meta<typeof CalendarEvent> = {
-//   title: 'Example/CalendarEvent',
-//   component: CalendarEvent,
-//   tags: ['autodocs'],
-// };
+const meta: Meta<typeof CalendarEvent> = {
+  title: "Example/CalendarEvent",
+  component: CalendarEvent,
+  tags: ["autodocs"],
+};
 
-// export default meta;
-// type Story = StoryObj<typeof CalendarEvent>;
+export default meta;
+type Story = StoryObj<typeof CalendarEvent>;
 
-// const defaultArgs: CalendarEventProps = {
-//   event: {
-//     allDay: false,
-//     title: 'New Event',
-//     start: new Date(),
-//     end: new Date(),
-//     resource: {
-//       id: 'event1',
-//       completed: false,
-//     },
-//   },
-// };
+const defaultArgs: CalendarEventProps = {
+  event: {
+    allDay: false,
+    title: "New Event",
+    start: new Date(),
+    end: new Date(),
+    resource: {
+      id: "event1",
+      completed: false,
+      type: "event",
+    },
+  },
+};
 
-// export const Default: Story = {
-//   args: defaultArgs,
-// };
+export const Default: Story = {
+  args: defaultArgs,
+};
 
-// const completedArgs: CalendarEventProps = {
-//   event: {
-//     allDay: false,
-//     title: 'New Event',
-//     start: new Date(),
-//     end: new Date(),
-//     resource: {
-//       id: 'event1',
-//       completed: true,
-//     },
-//   },
-// };
+const completedArgs: CalendarEventProps = {
+  event: {
+    allDay: false,
+    title: "New Event",
+    start: new Date(),
+    end: new Date(),
+    resource: {
+      id: "event1",
+      completed: true,
+      type: "event",
+    },
+  },
+};
 
-// export const Completed: Story = {
-//   args: completedArgs,
-// };
+export const Completed: Story = {
+  args: completedArgs,
+};
