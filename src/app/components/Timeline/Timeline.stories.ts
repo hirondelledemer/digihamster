6d71f0227c06b3600b66dd7db23d764cf110bdd0
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2024-09-04 00:10:23 +0300

    add todo

--- src/app/components/Timeline/Timeline.stories.ts
+++ src/app/components/Timeline/Timeline.stories.ts
@@ -1,12 +1,12 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Timeline from '.';
-import { TimelineProps } from './Timeline';
+import Timeline from ".";
+import { TimelineProps } from "./Timeline";
 
 const meta: Meta<typeof Timeline> = {
-  title: 'Example/Timeline',
+  title: "Example/Timeline",
   component: Timeline,
-  tags: ['autodocs']
+  tags: ["autodocs"],
 };
 
 export default meta;
@@ -14,6 +14,7 @@ type Story = StoryObj<typeof Timeline>;
 
 const defaultArgs: TimelineProps = {};
 
+// todo: add wrappers
 export const Default: Story = {
-  args: defaultArgs
+  args: defaultArgs,
 };
