cf9103d58c7e494cb787b7a5343458f4fb78ada7
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2024-09-07 01:01:04 +0300

    ..

--- src/app/components/CalendarWeatherEvent/CalendarWeatherEvent.spec.tsx
+++ src/app/components/CalendarWeatherEvent/CalendarWeatherEvent.spec.tsx
@@ -1,13 +1,31 @@
-import { render } from '@testing-library/react';
-import CalendarWeatherEvent, { CalendarWeatherEventProps } from './CalendarWeatherEvent';
-import { getCalendarWeatherEventTestkit } from './CalendarWeatherEvent.testkit';
+import { render } from "@testing-library/react";
+import CalendarWeatherEvent, {
+  CalendarWeatherEventProps,
+} from "./CalendarWeatherEvent";
+import { getCalendarWeatherEventTestkit } from "./CalendarWeatherEvent.testkit";
 
-describe('CalendarWeatherEvent', () => {
-  const defaultProps: CalendarWeatherEventProps = {};
+describe("CalendarWeatherEvent", () => {
+  const defaultProps: CalendarWeatherEventProps = {
+    event: {
+      title: "weather",
+      resource: {
+        type: "weather",
+        id: "id",
+        temp: 20,
+        weather: [
+          {
+            main: "Clear",
+          },
+        ],
+      },
+    },
+  };
   const renderComponent = (props = defaultProps) =>
-    getCalendarWeatherEventTestkit(render(<CalendarWeatherEvent {...props} />).container);
+    getCalendarWeatherEventTestkit(
+      render(<CalendarWeatherEvent {...props} />).container
+    );
 
-  it('should render CalendarWeatherEvent', () => {
+  it("should render CalendarWeatherEvent", () => {
     const wrapper = renderComponent();
     expect(wrapper.getComponent()).not.toBe(null);
   });
