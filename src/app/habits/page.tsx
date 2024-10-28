507d0f55c74e081112f976eb1afa38a518fa1582
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2024-10-28 16:43:31 +0200

    [fix] add command tool to habits page

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,11 +1,19 @@
+import CommandTool from "../components/CommandTool";
 import Habits from "../components/Habits";
 
 import { HabitsContextProvider } from "../utils/hooks/use-habits";
+import { ProjectsContextProvider } from "../utils/hooks/use-projects";
+import { TasksContextProvider } from "../utils/hooks/use-tasks";
 
 export default function HabitsPage() {
   return (
     <HabitsContextProvider>
-      <Habits />
+      <TasksContextProvider>
+        <ProjectsContextProvider>
+          <Habits />
+          <CommandTool />
+        </ProjectsContextProvider>
+      </TasksContextProvider>
     </HabitsContextProvider>
   );
 }
