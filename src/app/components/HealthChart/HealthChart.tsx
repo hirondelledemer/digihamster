52198350ded760c82da5edcc34a9e768bd3504b9
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2024-10-29 15:39:51 +0200

    [fix] show last month data in health check

--- src/app/components/HealthChart/HealthChart.tsx
+++ src/app/components/HealthChart/HealthChart.tsx
@@ -6,7 +6,7 @@ import { ChartContainer, ChartTooltip, ChartTooltipContent } from "../ui/chart";
 import { Card, CardContent } from "../ui/card";
 import useHabits from "@/app/utils/hooks/use-habits";
 import { now } from "@/app/utils/date/date";
-import { DAY } from "@/app/utils/consts/dates";
+import { subDays } from "date-fns";
 
 export interface HealthChartProps {
   testId?: string;
@@ -15,6 +15,7 @@ export interface HealthChartProps {
 interface ChartItem {
   item: string;
   value: number;
+  lastMonthValue: number;
   label: ReactNode;
 }
 
@@ -33,7 +34,9 @@ const HealthChart: FC<HealthChartProps> = (): JSX.Element => {
             return curr.timesPerMonth + prev;
           }, 0);
 
-          const earliestDay = now().getTime() - 28 * DAY;
+          const earliestDay = subDays(now(), 28).getTime();
+
+          const lastMonthEarliestDay = subDays(now(), 56).getTime();
 
           const allTheProgress = habitsForCategory.map((habit) => {
             const progress = habit.log.filter(
@@ -49,12 +52,28 @@ const HealthChart: FC<HealthChartProps> = (): JSX.Element => {
                 .length + curr
             );
           }, 0);
+
+          const lastMonthProgress = habitsForCategory.reduce((curr, prev) => {
+            return (
+              prev.log.filter(
+                (log) =>
+                  log.at >= lastMonthEarliestDay &&
+                  log.at < earliestDay &&
+                  log.completed
+              ).length + curr
+            );
+          }, 0);
+
           const progressPercentage = (progress / total) * 100;
+
+          const lastMonthProgressPercentage = (lastMonthProgress / total) * 100;
+
           return [
             ...prev,
             {
               item: curr,
               value: Math.floor(progressPercentage),
+              lastMonthValue: Math.floor(lastMonthProgressPercentage),
               label: (
                 <div className="mr-2">
                   {allTheProgress.map((pr) => (
@@ -74,20 +93,30 @@ const HealthChart: FC<HealthChartProps> = (): JSX.Element => {
     <Card>
       <CardContent className="pb-0">
         <ChartContainer
-          config={{}}
+          config={{
+            value: {
+              color: "hsl(var(--chart-5))",
+            },
+            lastMonthValue: {
+              label: "Last month",
+              color: "hsl(var(--chart-2))",
+            },
+          }}
           className="mx-auto aspect-square max-h-[300px]"
           height={300}
         >
           <RadarChart data={chartData}>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideIndicator />}
-            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <PolarAngleAxis dataKey="item" />
             <PolarGrid />
             <Radar
               dataKey="value"
-              fill="hsl(var(--chart-5))"
+              fill="var(--color-value)"
+              fillOpacity={0.6}
+            />
+            <Radar
+              dataKey="lastMonthValue"
+              fill="var(--color-lastMonthValue)"
               fillOpacity={0.6}
             />
           </RadarChart>
