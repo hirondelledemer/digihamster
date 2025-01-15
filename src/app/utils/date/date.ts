66c6071405c7623334c2430c055937fd2deebeef
Meda <hirondelledemer4@gmail.com>
2025-01-15 14:41:31 +0200

    Revert "[feat] redo habits (#32)" (#33)
    This reverts commit 1561c0e67823b0581b8f454504ac119977c87d89.

--- src/app/utils/date/date.ts
+++ src/app/utils/date/date.ts
@@ -1,18 +1,1 @@
-import { subDays } from "date-fns";
-
 export const now = () => new Date();
-
-export const getTodayWithZeroHours = () => {
-  const today = now();
-  today.setHours(0, 0, 0, 0);
-  return today;
-};
-
-// TODO: add commentary. Maybe rename
-export const getTimestampsFrom = (startingDate: Date, howMany: number) => {
-  let array = [];
-  for (let i = 0; i <= howMany; i++) {
-    array.push(subDays(startingDate, i).getTime());
-  }
-  return array.reverse();
-};
