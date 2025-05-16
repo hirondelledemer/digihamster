51d64a75acf61479d5ac4d838b65ba8768a41af4
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2025-05-16 14:52:29 +0300

    [feat] change wiki link

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -1,7 +1,7 @@
 export default function HabitsPage() {
   return (
     <iframe
-      src="https://digihamster-wiki-un7z.vercel.app/wiki.html"
+      src="https://digihamster-wiki.vercel.app/wiki.html"
       style={{ width: "100%", height: "100vh" }}
       title="Private Wiki"
     />
