ab98899363d279691e93320f3a1b76b458b8fa76
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2025-05-26 11:38:29 +0300

    mock date for timeline story

--- src/app/components/Timeline/Timeline.stories.ts
+++ src/app/components/Timeline/Timeline.stories.ts
@@ -3,6 +3,10 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Timeline from ".";
 import { TimelineProps } from "./Timeline";
 
+import { now } from "#src/app/utils/date/date.mock";
+
+const TODAY_DATE = new Date("2024-02-14");
+
 const meta: Meta<typeof Timeline> = {
   title: "Example/Timeline",
   component: Timeline,
@@ -16,5 +20,9 @@ const defaultArgs: TimelineProps = {};
 
 // todo: add wrappers
 export const Default: Story = {
+  async beforeEach() {
+    now.mockReturnValue(TODAY_DATE);
+  },
+
   args: defaultArgs,
 };
