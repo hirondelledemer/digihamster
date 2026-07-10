e9be2f3c3534739c12d68c9c5006885c072a7602
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2026-07-10 14:21:46 +0300

    ..

--- src/app/api/cycle/route.ts
+++ src/app/api/cycle/route.ts
@@ -1,10 +1,10 @@
 import { getDataFromToken } from "@/app/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
-import Cycle, { ICycle } from "@/models/cycle";
-import { addDays, differenceInCalendarDays } from "date-fns";
+import Cycle from "@/models/cycle";
+// import { differenceInCalendarDays } from "date-fns";
 import { connect } from "@/config/database/connection";
 
-const DAY = 1000 * 60 * 60 * 24;
+// const DAY = 1000 * 60 * 60 * 24;
 
 connect();
 
@@ -20,75 +20,75 @@ export async function GET(request: NextRequest) {
   }
 }
 
-export async function PATCH(request: NextRequest) {
-  try {
-    const userId = await getDataFromToken(request);
-    const reqBody = await request.json();
-    const args = reqBody;
+// export async function PATCH(request: NextRequest) {
+//   try {
+//     const userId = await getDataFromToken(request);
+//     const reqBody = await request.json();
+//     const args = reqBody;
 
-    const cycle = await Cycle.findOne<ICycle>({ userId });
+//     const cycle = await Cycle.findOne<ICycle>({ userId });
 
-    if (!cycle) {
-      const newCycle = new Cycle({
-        userId,
-        dates: [
-          {
-            startDate: args.startDate,
-            endDate: args.startDate + DAY * 5,
-          },
-        ],
-      });
+//     if (!cycle) {
+//       const newCycle = new Cycle({
+//         userId,
+//         dates: [
+//           {
+//             startDate: args.startDate,
+//             endDate: args.startDate + DAY * 5,
+//           },
+//         ],
+//       });
 
-      const savedCycle = await newCycle.save();
-      return NextResponse.json<ICycle>(savedCycle);
-    }
+//       const savedCycle = await newCycle.save();
+//       return NextResponse.json<ICycle>(savedCycle);
+//     }
 
-    const futureDates = [];
+//     const futureDates = [];
 
-    if (cycle.dates.length > 2) {
-      const delta = averageDelta(cycle.dates);
+//     if (cycle.dates.length > 2) {
+//       const delta = averageDelta(cycle.dates);
 
-      for (let i = 1; i <= 10; i++) {
-        futureDates.push({
-          startDate: addDays(args.startDate, delta * i).getTime(),
-          endDate: addDays(args.startDate, delta * i + 5).getTime(),
-        });
-      }
-    }
+//       for (let i = 1; i <= 10; i++) {
+//         futureDates.push({
+//           startDate: addDays(args.startDate, delta * i).getTime(),
+//           endDate: addDays(args.startDate, delta * i + 5).getTime(),
+//         });
+//       }
+//     }
 
-    const updatedCycle = await Cycle.findOneAndUpdate<ICycle>(
-      {
-        userId,
-      },
-      {
-        futureDates: futureDates,
-        $push: {
-          dates: {
-            startDate: args.startDate,
-            endDate: args.startDate + DAY * 5,
-          },
-        },
-      }
-    );
-    return NextResponse.json(updatedCycle);
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
-  }
-}
+//     const updatedCycle = await Cycle.findOneAndUpdate<ICycle>(
+//       {
+//         userId,
+//       },
+//       {
+//         futureDates: futureDates,
+//         $push: {
+//           dates: {
+//             startDate: args.startDate,
+//             endDate: args.startDate + DAY * 5,
+//           },
+//         },
+//       }
+//     );
+//     return NextResponse.json(updatedCycle);
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 400 });
+//   }
+// }
 
-const averageDelta = ([x, ...xs]: {
-  startDate: number;
-  endDate: number;
-}[]) => {
-  if (x.startDate === undefined) return NaN;
-  else
-    return (
-      xs.reduce(
-        ([acc, last], x) => [
-          acc + differenceInCalendarDays(x.startDate, last),
-          x.startDate,
-        ],
-        [0, x.startDate]
-      )[0] / xs.length
-    );
-};
+// const averageDelta = ([x, ...xs]: {
+//   startDate: number;
+//   endDate: number;
+// }[]) => {
+//   if (x.startDate === undefined) return NaN;
+//   else
+//     return (
+//       xs.reduce(
+//         ([acc, last], x) => [
+//           acc + differenceInCalendarDays(x.startDate, last),
+//           x.startDate,
+//         ],
+//         [0, x.startDate],
+//       )[0] / xs.length
+//     );
+// };
