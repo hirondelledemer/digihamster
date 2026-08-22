bb3c080cfa77657304f9dec328b6e4c40446c24e
hirondelledemer <hirondelledemer4@gmail.com>
2026-08-22 20:32:32 +0300

    [feat] indicate event status

--- src/app/components/CalendarEvent/CalendarEvent.tsx
+++ src/app/components/CalendarEvent/CalendarEvent.tsx
@@ -10,6 +10,7 @@ import {
 import { useProjectsState } from "@/app/utils/hooks/use-projects/state-context";
 import { EventActions } from "../EventActions";
 import { format } from "date-fns";
+import { EventStatus } from "@/app/utils/types/event";
 
 export interface CalendarEventProps {
   event: CalendarEventEntry;
@@ -71,27 +72,53 @@ const CalendarEvent: FC<CalendarEventProps> = ({
     ? `color-mix(in srgb, ${project.color} 20%, transparent)`
     : "";
 
+  const getEventStatusColor = () => {
+    if (event.resource.event.status === EventStatus.Completed) {
+      return "repeating-linear-gradient(45deg, hsl(var(--scale-color-10)/0.4), hsl(var(--scale-color-10)/0.5) 3px, hsl(var(--scale-color-10)/0.1) 5px, hsl(var(--scale-color-10)/0.1) 20px)";
+    }
+    if (event.resource.event.status === EventStatus.Cancelled) {
+      return "repeating-linear-gradient(45deg, hsl(var(--scale-color-1)/0.4), hsl(var(--scale-color-1)/0.5) 3px, hsl(var(--scale-color-1)/0.1) 5px, hsl(var(--scale-color-1)/0.1) 20px)";
+    }
+    if (event.resource.event.status === EventStatus.Moved) {
+      return "repeating-linear-gradient(45deg, hsl(var(--scale-color-5)/0.4), hsl(var(--scale-color-5)/0.5) 3px, hsl(var(--scale-color-5)/0.1) 5px, hsl(var(--scale-color-5)/0.1) 20px)";
+    }
+
+    return "";
+  };
+
   return (
-    <div
-      ref={containerRef}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        backgroundColor: projectColor || "#29221f",
-        border: `2px solid ${projectColor || "hsl(var(--primary)/0.5)"}`,
-      }}
-      className="h-full p-1 cursor-pointer rounded-lg relative "
-    >
-      <div className="text-xs absolute top-[-20px]">
-        {format(event.start, "HH:mm")}
+    <>
+      <div
+        ref={containerRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          backgroundColor: projectColor || "#29221f",
+          // backgroundColor: getEventStatusColor(),
+
+          border: `2px solid ${projectColor || "hsl(var(--primary)/0.5)"}`,
+        }}
+        className="h-full p-1 cursor-pointer rounded-lg relative "
+      >
+        <EventActions event={event.resource.event}>
+          <div
+            className="h-full absolute top-0 bottom-0 left-0 right-0 rounded-md m-[1px]"
+            style={{
+              background: getEventStatusColor(),
+            }}
+          />
+        </EventActions>
+        <div className="text-xs absolute top-[-20px]">
+          {format(event.start, "HH:mm")}
+        </div>
+        <EventActions event={event.resource.event} triggerClassName="h-full">
+          {content}
+        </EventActions>
+        <div className="text-xs absolute bottom-[-20px]">
+          {event.end && format(event.end, "HH:mm")}
+        </div>
       </div>
-      <EventActions event={event.resource.event} triggerClassName="h-full">
-        {content}
-      </EventActions>
-      <div className="text-xs absolute bottom-[-20px]">
-        {event.end && format(event.end, "HH:mm")}
-      </div>
-    </div>
+    </>
   );
 };
 
